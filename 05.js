06ea85d29c16e93958a0cef3abccf85732abf352
RidersWeb <samara_expert@mail.ru>
2021-05-05 10:43:11 +0300

    commit 2

--- 05.js
+++ 05.js
@@ -10,23 +10,38 @@ let point = {
   h: 100,
   w: 150
 }
-let point1 = {};
+console.log('1. Создан объект point', point)
 
-// объявляем функцию
-function obj(object){
-  for(let key in object){
+let point1 = {}; // создаем пустой объект
+
+// объявляем функцию перебераем и копируем объект
+function obj(object, target) {
+  console.log('2. Функция переберает и копирует объект по необходимости')
+  for (let key in object) {
     console.log(`${key} - ` + object[key]);
-    point1[key] = object[key]
+    if (target) {
+      target[key] = object[key];
+    }
+
   }
 }
-
-obj(point) //Вызываем функцию
+obj(point, point1) //Вызываем функцию
 
 // изменяем значения оъекта point
 point.x = 35;
 point.h = 45;
 point.w = 65;
 
-console.log(point)
+console.log('3. Выводим измененый point', point);
 
-obj(point) // еще раз вызываем функцию, объект уже изменен.
+console.log('4. Выводим point1', point1)
+
+obj(point, ) // еще раз вызываем функцию, объект уже изменен.
+
+// есть встроенный метод
+
+let point2 = {}; // Создаем пустой объект
+
+Object.assign(point2, point);
+
+console.log('5. Выводим новый объект копия point', point2);
