b2b6089f23ed14081bc81c7e7cd6eed06b6bd8db
RidersWeb <samara_expert@mail.ru>
2021-05-18 21:30:03 +0300

    commit 16

---
+++ 16.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector('button');
+let input = document.querySelector('input').value;
+let result = document.querySelector('#result').textContent;
+
+
+
+
+
+console.log(btn)
+console.log(input)
+console.log(result)
